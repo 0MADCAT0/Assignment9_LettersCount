e59c3abd6c7a685690d49009f75dfa13073d5caa
MADCAT <orioncloud2020@gmail.com>
2020-07-22 12:15:07 +0300

    first commit

---
+++ Assignment9_LettersCount.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 22 11:48:58 2020
+
+@author: MADCAT
+"""
+
+
+sntnc = str(input("input your sentence:"))
+
+
+def letr_count(s):
+    """counts every single char in given string"""
+    letr_count = dict()
+    a = set(s)
+    for i in a:
+        letr_count[i] = s.count(i)
+    return letr_count
+
+
+print(letr_count(sntnc))
+   
